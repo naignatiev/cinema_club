9dbfc44f198464cd33278949fdc4793e6c4cb578
Nikolay Ignatiev <naignatiev@yandex.ru>
2024-01-14 21:05:22 +0300

    django extensions

--- config/settings.py
+++ config/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'movies.apps.MoviesConfig',
-    'debug_toolbar'
+    'debug_toolbar',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
