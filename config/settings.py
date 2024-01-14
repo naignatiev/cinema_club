0fa092f48cf3f2318f6dc83460ca04117a6a1f10
Nikolay Ignatiev <naignatiev@yandex.ru>
2024-01-14 16:49:24 +0300

    nginx in docker-compose

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False) == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'django']
 
 
 # Application definition
@@ -126,6 +126,4 @@ include('components/database.py')
 LOCALE_PATHS = ['movies/locale']
 
 INTERNAL_IPS = [
-    '127.0.0.1',
-    '0.0.0.0'
 ]
